0abf86a96f14a26dbc5bb7159c6838e31e2dc5ea
Jacob Hilton <jacob@alignment.org>
2023-06-09 14:56:17 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="heuristic_hafnian",
+    packages=find_packages(),
+    version="0.0.1",
+    install_requires=["numpy", "thewalrus"],
+    extras_require={},
+)
