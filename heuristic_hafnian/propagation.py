77e83ca46d7b372e9ded2d4f30a5f289104d6bf2
Jacob Hilton <jacob@alignment.org>
2023-07-07 11:31:55 -0700

    permnanent and factored permanent

--- heuristic_hafnian/propagation.py
+++ heuristic_hafnian/propagation.py
@@ -195,3 +195,198 @@ def cumulant_propagation_with_imputation(
         return sum_over_partitions(pairs, singles, cumulant_fn)
 
     return cumulant_fn(variables)
+
+
+import numpy as np
+import itertools
+
+
+def symmetrize(a):
+    assert a.ndim % 2 == 0
+    b = 0
+    for perm in itertools.permutations(range(a.ndim // 2)):
+        b += a.transpose(perm + tuple(range(a.ndim // 2, a.ndim)))
+    c = 0
+    for perm in itertools.permutations(range(b.ndim // 2, b.ndim)):
+        c += b.transpose(tuple(range(b.ndim // 2)) + perm)
+    return c / (np.math.factorial(b.ndim // 2)) ** 2
+
+
+def permanent_old(covariance):
+    cov = covariance
+    cov2 = symmetrize(2 * np.tensordot(cov, cov, axes=0).transpose((0, 2, 1, 3)))
+
+    while cov.size > 1:
+        n = cov2.shape[0]
+        cov4 = symmetrize(
+            18 * np.tensordot(cov2, cov2, axes=0).transpose((0, 1, 4, 5, 2, 3, 6, 7))
+            - 48
+            * np.tensordot(
+                np.tensordot(cov, cov, axes=0),
+                np.tensordot(cov, cov, axes=0),
+                axes=0,
+            ).transpose((0, 2, 4, 6, 1, 3, 5, 7))
+        )
+        cov = cov2[np.arange(0, n, 2), np.arange(1, n, 2), :, :][
+            :, np.arange(0, n, 2), np.arange(1, n, 2)
+        ]
+        cov2 = cov4[np.arange(0, n, 2), np.arange(1, n, 2), :, :, :, :, :, :][
+            :, np.arange(0, n, 2), np.arange(1, n, 2), :, :, :, :
+        ][:, :, np.arange(0, n, 2), np.arange(1, n, 2), :, :][
+            :, :, :, np.arange(0, n, 2), np.arange(1, n, 2)
+        ]
+
+    return cov.item()
+
+
+import numpy as np
+import itertools
+
+
+def symmetrize_left(a):
+    assert a.ndim % 2 == 0
+    result = 0
+    for perm in itertools.permutations(range(a.ndim // 2)):
+        result += a.transpose(perm + tuple(range(a.ndim // 2, a.ndim)))
+    return result / np.math.factorial(a.ndim // 2)
+
+
+def symmetrize_right(a):
+    assert a.ndim % 2 == 0
+    result = 0
+    for perm in itertools.permutations(range(a.ndim // 2, a.ndim)):
+        result += a.transpose(tuple(range(a.ndim // 2)) + perm)
+    return result / np.math.factorial(a.ndim // 2)
+
+
+def next_level(cov):
+    n = cov.shape[0]
+    return cov[np.arange(0, n, 2), np.arange(1, n, 2), :, :, :, :, :, :][
+        :, np.arange(0, n, 2), np.arange(1, n, 2), :, :, :, :
+    ][:, :, np.arange(0, n, 2), np.arange(1, n, 2), :, :][
+        :, :, :, np.arange(0, n, 2), np.arange(1, n, 2)
+    ]
+
+
+def symmetric_power(mat, exp):
+    n = mat.ndim
+    power = functools.reduce(lambda a, b: np.tensordot(a, b, axes=0), (mat,) * exp)
+    power = power.transpose(tuple(i + j * n for i in range(n) for j in range(exp)))
+    for dim in range(n):
+        accum = 0
+        for perm in itertools.permutations(range(dim * exp, (dim + 1) * exp)):
+            accum += power.transpose(
+                tuple(range(dim * exp)) + perm + tuple(range((dim + 1) * exp, n * exp))
+            )
+        power = accum / np.math.factorial(exp)
+    return power
+
+
+def permanent(covariance):
+    cov = covariance
+    cov2 = 2 * symmetric_power(cov, 2)
+
+    while cov.size > 1:
+        n = cov2.shape[0]
+        cov4 = symmetrize_left(
+            symmetrize_right(
+                18
+                * np.tensordot(cov2, cov2, axes=0).transpose((0, 1, 4, 5, 2, 3, 6, 7))
+            )
+        ) - 48 * symmetric_power(cov, 4)
+        cov = cov2[np.arange(0, n, 2), np.arange(1, n, 2), :, :][
+            :, np.arange(0, n, 2), np.arange(1, n, 2)
+        ]
+        cov2 = cov4[np.arange(0, n, 2), np.arange(1, n, 2), :, :, :, :, :, :][
+            :, np.arange(0, n, 2), np.arange(1, n, 2), :, :, :, :
+        ][:, :, np.arange(0, n, 2), np.arange(1, n, 2), :, :][
+            :, :, :, np.arange(0, n, 2), np.arange(1, n, 2)
+        ]
+
+    return cov.item()
+
+
+def symmetric_left_power(mat, exp):
+    n = mat.ndim
+    power = functools.reduce(lambda a, b: np.tensordot(a, b, axes=0), (mat,) * exp)
+    power = power.transpose(tuple(i + j * n for i in range(n) for j in range(exp)))
+    power = power.reshape(power.shape[: (n - 1) * exp] + (-1,))
+    result = 0
+    for perm in itertools.permutations(range((n - 1) * exp)):
+        result += power.transpose(perm + ((n - 1) * exp,))
+    result = result / np.math.factorial((n - 1) * exp) ** 0.5
+    return result
+
+
+def symmetric_left_power2(mat, exp):
+    n = mat.ndim
+    power = functools.reduce(lambda a, b: np.tensordot(a, b, axes=0), (mat,) * exp)
+    power = power.transpose(tuple(i + j * n for i in range(n) for j in range(exp)))
+    power = power.reshape(power.shape[: (n - 1) * exp] + (-1,))
+    result = 0
+    # for perm in [(0, 1, 2, 3), (0, 1, 3, 2), (0, 2, 1, 3), (1, 0, 2, 3), (1, 0, 3, 2), (3, 1, 0, 2)]:
+    #     result += power.transpose(perm + ((n - 1) * exp,))
+    for perm in [(0, 1, 2, 3), (1, 0, 3, 2)]:
+        result += power.transpose(perm + ((n - 1) * exp,))
+    power = result
+    result = 0
+    for perm in [
+        (0, 1, 2, 3),
+        (1, 0, 2, 3),
+        (0, 2, 1, 3),
+    ]:  # [(0, 1, 2, 3), (1, 0, 2, 3), (2, 0, 1, 3)]:
+        result += power.transpose(perm + ((n - 1) * exp,))
+    result = result / 6**0.5  # np.math.factorial((n - 1) * exp)
+    return result
+
+
+def split(cov):
+    n1, n2 = np.prod(cov.shape[: cov.ndim // 2]), np.prod(cov.shape[cov.ndim // 2 :])
+    U, S, Vh = np.linalg.svd(cov.reshape(n1, n2))
+    u = (U @ np.diag(S**0.5)).reshape(cov.shape[: cov.ndim // 2] + (-1,))
+    v = (Vh.T @ np.diag(S**0.5)).reshape(cov.shape[cov.ndim // 2 :] + (-1,))
+    return u, v
+
+
+def project(u, v):
+    cov = np.tensordot(u, v, axes=((-1), (-1)))
+    return split(cov)
+
+
+def factored_permanent(covariance):
+    cov = covariance
+    u, v = split(cov)
+    u2 = symmetric_left_power(u, 2)
+    v2 = symmetric_left_power(v, 2)
+
+    while u.size > 1:
+        n = u.shape[0]
+
+        u4 = np.concatenate(
+            [
+                3**0.5 * symmetric_left_power2(u2, 2),
+                2**0.5 * 1j * symmetric_left_power2(symmetric_left_power(u, 2), 2),
+            ],
+            axis=-1,
+        )
+        v4 = np.concatenate(
+            [
+                3**0.5 * symmetric_left_power2(v2, 2),
+                2**0.5 * 1j * symmetric_left_power2(symmetric_left_power(v, 2), 2),
+            ],
+            axis=-1,
+        )
+
+        u = u2[np.arange(0, n, 2), np.arange(1, n, 2), :]
+        v = v2[np.arange(0, n, 2), np.arange(1, n, 2), :]
+        u2 = u4[np.arange(0, n, 2), np.arange(1, n, 2), :, :, :][
+            :, np.arange(0, n, 2), np.arange(1, n, 2), :
+        ]
+        v2 = v4[np.arange(0, n, 2), np.arange(1, n, 2), :, :, :][
+            :, np.arange(0, n, 2), np.arange(1, n, 2), :
+        ]
+
+        u, v = project(u, v)
+        u2, v2 = project(u2, v2)
+
+    return np.real(u @ v.transpose()).item()
